24c32f54411fa62ab828648e23617083c823e380
judithMarg <judithmargaritapacollalli@gmail.com>
2024-10-20 17:21:46 -0400

    add MovieActivity ui more exactly

--- app/src/main/java/com/example/proyectofinal/MovieActivity.kt
+++ app/src/main/java/com/example/proyectofinal/MovieActivity.kt
@@ -16,24 +16,25 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.Star
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -52,10 +53,8 @@ import coil.compose.AsyncImage
 import com.example.proyectofinal.ui.theme.AppTheme
 import com.example.proyectofinal.ui.theme.onPrimaryContainerLight
 import com.example.proyectofinal.ui.theme.onPrimaryLight
-import com.example.proyectofinal.ui.theme.outlineLight
 import com.example.proyectofinal.ui.theme.outlineLightHighContrast
 import com.example.proyectofinal.ui.theme.primaryContainerLightMediumContrast
-import com.example.proyectofinal.ui.theme.secondaryContainerLight
 import com.example.proyectofinal.ui.theme.tertiaryCommon
 
 class MovieActivity : ComponentActivity() {
@@ -64,7 +63,6 @@ class MovieActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     MovieScreen(
                         name = "Interestelar",
                         image = "https://m.media-amazon.com/images/S/pv-target-images/79194981293eabf6620ece96eb5a9c1fffa04d3374ae12986e0748800b37b9cf.jpg",
@@ -74,121 +72,141 @@ class MovieActivity : ComponentActivity() {
                                 "exploradores, encabezadis por Cooper, se embarcan en un viaje " +
                                 "espacial para encontrar un lugar con las condiciones necesarias" +
                                 "para reemplazar a la Tierra y comenzar una nueva vida alli.",
-                        modifier = Modifier.padding(innerPadding)
                     )
-                }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MovieScreen(name: String, image: String, subtitle: String, points:String, descrip: String, modifier: Modifier = Modifier) {
+fun MovieScreen(name: String, image: String, subtitle: String, points:String, descrip: String) {
     val movieImage = image
     var iconSelect by remember { mutableStateOf(false) }
     var rating by remember { mutableStateOf(0) }
 
-    Box(modifier = modifier
-        .background(primaryContainerLightMediumContrast)
-        .padding(horizontal = 26.dp, vertical = 30.dp)
-        .fillMaxSize()
-        .clip(RoundedCornerShape(25.dp))){
-        Column(
-            modifier = Modifier
-                .background(onPrimaryContainerLight)
-                .fillMaxSize()
-                .padding(12.dp)
-                .verticalScroll(rememberScrollState()),
-            verticalArrangement = Arrangement.spacedBy(15.dp)
-        ) {
-            AsyncImage(model = image, contentDescription = null, modifier= Modifier
-                .height(340.dp)
-                .clip(RoundedCornerShape(16.dp)),
-                contentScale = ContentScale.Crop
-            )
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = name,
-                    style = MaterialTheme.typography.bodyLarge,
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.SemiBold,
-                    color = onPrimaryLight
-                )
-                Button(
-                    onClick = { iconSelect = !iconSelect},
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.Transparent
-                    )
-                ) {
-                    Icon(
-                        imageVector = if (iconSelect) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
-                        contentDescription = "Favoritos",
-                        modifier = Modifier.size(34.dp),
-                        tint = onPrimaryLight
-                    )
-                }
-            }
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = subtitle,
-                    style = MaterialTheme.typography.bodySmall,
-                    color = onPrimaryLight,
-                    fontSize = 15.sp
-                )
-                Text(
-                    text = points,
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = tertiaryCommon,
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Medium
-                )
-            }
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                RatingBar(
-                    rating = rating,
-                    onRatingChanged = { newRating ->
-                        rating = newRating
+    Scaffold(modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { /*TODO*/ },
+                colors = TopAppBarDefaults.topAppBarColors(primaryContainerLightMediumContrast),
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            modifier = Modifier.size(34.dp),
+                            contentDescription = ""
+                        )
                     }
+                }
+            )
+        }
+        ) { innerPadding ->
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(primaryContainerLightMediumContrast)
+            .padding(innerPadding)
+            .padding(start = 26.dp, bottom = 30.dp, end = 26.dp)
+            .clip(RoundedCornerShape(25.dp))
+        ){
+            Column(
+                modifier = Modifier
+                    .background(onPrimaryContainerLight)
+                    .fillMaxSize()
+                    .padding(12.dp)
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.spacedBy(15.dp)
+            ) {
+                AsyncImage(model = image, contentDescription = null, modifier= Modifier
+                    .height(340.dp)
+                    .clip(RoundedCornerShape(16.dp)),
+                    contentScale = ContentScale.Crop
                 )
-                OutlinedButton(
-                    onClick = { /*TODO*/ },
-                    modifier = Modifier.fillMaxWidth()
-                        .clip(RoundedCornerShape(15.dp))
-                        .padding(horizontal = 10.dp)
-                        .height(40.dp),
-                    border = BorderStroke(1.dp, onPrimaryLight),
-                    shape = RoundedCornerShape(20.dp)
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
                     Text(
-                        text = stringResource(id = R.string.btn_ver_comentarios),
+                        text = name,
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = onPrimaryLight
+                    )
+                    IconButton(
+                        onClick = { iconSelect = !iconSelect},
+                        colors = IconButtonDefaults.iconButtonColors(
+                            containerColor = Color.Transparent
+                        )
+                    ) {
+                        Icon(
+                            imageVector = if (iconSelect) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                            contentDescription = "Favoritos",
+                            modifier = Modifier.size(34.dp),
+                            tint = onPrimaryLight
+                        )
+                    }
+                }
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = subtitle,
+                        style = MaterialTheme.typography.bodySmall,
                         color = onPrimaryLight,
-                        fontSize = 16.sp
+                        fontSize = 15.sp
+                    )
+                    Text(
+                        text = points,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = tertiaryCommon,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Medium
                     )
                 }
-            }
 
-            Text(
-                text = descrip,
-                color = onPrimaryLight,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 16.dp)
-            )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RatingBar(
+                        rating = rating,
+                        onRatingChanged = { newRating ->
+                            rating = newRating
+                        }
+                    )
+                    OutlinedButton(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clip(RoundedCornerShape(15.dp))
+                            .padding(start = 10.dp)
+                            .height(40.dp),
+                        border = BorderStroke(1.dp, onPrimaryLight),
+                        shape = RoundedCornerShape(20.dp)
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.btn_ver_comentarios),
+                            color = onPrimaryLight,
+                            fontSize = 16.sp
+                        )
+                    }
+                }
+
+                Text(
+                    text = descrip,
+                    color = onPrimaryLight,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp)
+                )
+            }
         }
     }
 }
